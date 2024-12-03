9cf60f1afc2b7ded1de554d114cc51bf5d3901db
Aysel Bilmez <aybilmez@gmail.com>
2024-12-03 12:15:31 +0300

    -

---
+++ IlkMVCProjesi/Models/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IlkMVCProjesi.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public string WelcomeMessage { get; set; } // Ekstra bir veri
+    }
+}
---
+++ Pratik2/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pratik2.Models;
+
+namespace Pratik2.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Aysel",
+                LastName = "Bilmez",
+                Email = "BongBong@gmail.com"
+            };
+
+            var orders = new List<Order>
+            {
+                new Order {Id= 1, ProductName = "Bilgisayar", Price = 33000, Quantity = 1},
+                new Order {Id= 2, ProductName = "Tablet", Price = 7000, Quantity=1},
+                new Order {Id= 3, ProductName = "fare", Price = 300, Quantity = 2 }
+            };
+
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+
+            return View(viewModel);
+        }
+    }
+}
